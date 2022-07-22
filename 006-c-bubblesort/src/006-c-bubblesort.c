b05c2964bda7154aba404bf42c4a19aad59cb0b7
Carlos Martinez <ing.cemh.871208@gmail.com>
2022-07-22 12:35:04 -0400

    Added comment for bubble sort algorithm

--- 006-c-bubblesort/src/006-c-bubblesort.c
+++ 006-c-bubblesort/src/006-c-bubblesort.c
@@ -18,12 +18,18 @@ static void bubbleSort (unsigned char* array);
 
 int main(void)
 {
-	unsigned char array[SIZE] = {4,3,6,2,7,8,9,1,5,0};
+	unsigned char array[SIZE] = {9,3,6,2,7,8,4,1,5,0};
 	bubbleSort(array);
 	return EXIT_SUCCESS;
 }
 
 /*Bubble sort function implementation*/
+/*The number of iterations is 45*/
+/* n = array members
+ * m = n-1
+ * m(m+1)/2
+ * */
+
 static void bubbleSort (unsigned char* array)
 {
 	unsigned char counter = 0u;
@@ -31,7 +37,7 @@ static void bubbleSort (unsigned char* array)
 	unsigned char aux = 0u;
 	for(counter = 0u;counter < SIZE;counter++)
 	{
-		for(index = counter;index < SIZE;index++)
+		for(index = counter+1u;index < SIZE;index++)
 		{
 			if (*(array + counter) > *(array + index))
 			{
